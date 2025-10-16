a4b71315941c3f1ea0b9229874becc1ce71914fc
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-10-16 20:51:08 -0300

    Classe Author

---
+++ src/entities/Author.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Author extends Person {
+    private String nationality;
+
+    public Author() {
+        super();
+    }
+
+    public Author(String name, String email, String nationality) {
+        super(name, email);
+        this.nationality = nationality;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+}
