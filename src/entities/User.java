f5800efb9ebbe468bb25376e77d38633bbb92540
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-10-16 20:54:00 -0300

    Classe User

---
+++ src/entities/User.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class User extends Person {
+    private String phone;
+    private String nickName;
+
+    public User() {
+        super();
+    }
+
+    public User(String name, String email, String phone, String nickName) {
+        super(name, email);
+        this.phone = phone;
+        this.nickName = nickName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+}
