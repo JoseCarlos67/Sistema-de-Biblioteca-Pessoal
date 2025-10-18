7883f88422d8b67af2cee0a8938e7761043a02c8
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-10-18 18:50:17 -0300

    Registro de livro funcionando

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,13 @@
 package application;
 
+import entities.Author;
+import entities.Book;
 import entities.Library;
 import entities.User;
+import enums.BookGenre;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Program {
@@ -11,13 +16,13 @@ public class Program {
 
         Library myLibary = new Library();
 
-        registerUser(myLibary, scanner);
-
+//        registerUser(myLibary, scanner);
+        registerBook(myLibary, scanner);
         scanner.close();
     }
 
     public static void registerUser(Library myLibrary, Scanner scanner) {
-        System.out.println("=== Registro de Usuário +++");
+        System.out.println("=== Registro de Usuário ===");
         System.out.print("Nome: ");
         String nameUser = scanner.nextLine();
         System.out.print("E-mail: ");
@@ -30,5 +35,32 @@ public class Program {
         User newUser = new User(nameUser, emailUer, phoneNumerUser, nickNameUser);
 
         myLibrary.registerUser(newUser);
+    }
+
+    public static void registerBook(Library myLibrary, Scanner scanner) {
+        System.out.println("=== Registro de Livro ===");
+        System.out.print("Título: ");
+        String titleBook = scanner.nextLine();
+        System.out.print("Gênero: ");
+        BookGenre bookGenre = BookGenre.valueOf(scanner.nextLine());
+        System.out.print("Data de publicação: ");
+        String publishDateString = scanner.nextLine();
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate publishDate = LocalDate.parse(publishDateString, fmt1);
+
+        Author author = registerAuthor(scanner);
+
+        Book newBook = new Book(titleBook, bookGenre,publishDate, author);
+    }
+
+    public static Author registerAuthor(Scanner scanner) {
+        System.out.println("--- Registro de Autor ---");
+        System.out.print("Nome: ");
+        String nameAuthor = scanner.nextLine();
+        System.out.print("E-mail: ");
+        String emailAuthor = scanner.nextLine();
+        System.out.print("Nacionalidade: ");
+        String nationalityAuthor = scanner.nextLine();
+        return new Author(nameAuthor, emailAuthor, nationalityAuthor);
     }
 }
