0fabba2ad64e03c52a80e0fcc5da708751469154
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-10-20 11:59:38 -0300

    Funcoes classe Program: autofillingOfDate e registerLoanBook - funcionando

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,61 +6,94 @@ import entities.Library;
 import entities.User;
 import enums.BookGenre;
 
+import java.io.ByteArrayInputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    ByteArrayInputStream fakeInput = autofillingOfData();
+    System.setIn(fakeInput);
 
-        Library myLibary = new Library();
+    Scanner scanner = new Scanner(System.in);
 
-//        registerUser(myLibary, scanner);
-        registerBook(myLibary, scanner);
-        scanner.close();
-    }
+    Library myLibary = new Library();
 
-    public static void registerUser(Library myLibrary, Scanner scanner) {
-        System.out.println("=== Registro de Usuário ===");
-        System.out.print("Nome: ");
-        String nameUser = scanner.nextLine();
-        System.out.print("E-mail: ");
-        String emailUer = scanner.nextLine();
-        System.out.print("Telefone: ");
-        String phoneNumerUser = scanner.nextLine();
-        System.out.print("Apelido: ");
-        String nickNameUser = scanner.nextLine();
+    registerUser(myLibary, scanner);
+    registerBook(myLibary, scanner);
+    registerLoanBook(myLibary, scanner);
+    scanner.close();
+  }
 
-        User newUser = new User(nameUser, emailUer, phoneNumerUser, nickNameUser);
+  private static ByteArrayInputStream autofillingOfData() {
+    String fakeInputString = """
+            Larissa Borges
+            larissa@gmail.com
+            35991919191
+            Hokala
+            O senhor dos anéis
+            FANTASI
+            29/07/1954
+            J.R.R. Tolkien
+            tolkien@gmail.com
+            Britânico
+            O senhor dos anéis
+            Hokala
+            """;
+    return new ByteArrayInputStream(fakeInputString.getBytes());
+  }
 
-        myLibrary.registerUser(newUser);
-    }
+  public static void registerUser(Library myLibrary, Scanner scanner) {
+    System.out.println("=== Registro de Usuário ===");
+    System.out.print("Nome: ");
+    String nameUser = scanner.nextLine();
+    System.out.print("E-mail: ");
+    String emailUer = scanner.nextLine();
+    System.out.print("Telefone: ");
+    String phoneNumerUser = scanner.nextLine();
+    System.out.print("Apelido: ");
+    String nickNameUser = scanner.nextLine();
 
-    public static void registerBook(Library myLibrary, Scanner scanner) {
-        System.out.println("=== Registro de Livro ===");
-        System.out.print("Título: ");
-        String titleBook = scanner.nextLine();
-        System.out.print("Gênero: ");
-        BookGenre bookGenre = BookGenre.valueOf(scanner.nextLine());
-        System.out.print("Data de publicação: ");
-        String publishDateString = scanner.nextLine();
-        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate publishDate = LocalDate.parse(publishDateString, fmt1);
+    User newUser = new User(nameUser, emailUer, phoneNumerUser, nickNameUser);
 
-        Author author = registerAuthor(scanner);
+    myLibrary.registerUser(newUser);
+  }
 
-        Book newBook = new Book(titleBook, bookGenre,publishDate, author);
-    }
+  public static void registerBook(Library myLibrary, Scanner scanner) {
+    System.out.println("=== Registro de Livro ===");
+    System.out.print("Título: ");
+    String titleBook = scanner.nextLine();
+    System.out.print("Gênero: ");
+    BookGenre bookGenre = BookGenre.valueOf(scanner.nextLine());
+    System.out.print("Data de publicação: ");
+    String publishDateString = scanner.nextLine();
+    DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    LocalDate publishDate = LocalDate.parse(publishDateString, fmt1);
 
-    public static Author registerAuthor(Scanner scanner) {
-        System.out.println("--- Registro de Autor ---");
-        System.out.print("Nome: ");
-        String nameAuthor = scanner.nextLine();
-        System.out.print("E-mail: ");
-        String emailAuthor = scanner.nextLine();
-        System.out.print("Nacionalidade: ");
-        String nationalityAuthor = scanner.nextLine();
-        return new Author(nameAuthor, emailAuthor, nationalityAuthor);
-    }
+    Author author = registerAuthor(scanner);
+
+    Book newBook = new Book(titleBook, bookGenre, publishDate, author);
+  }
+
+  public static Author registerAuthor(Scanner scanner) {
+    System.out.println("--- Registro de Autor ---");
+    System.out.print("Nome: ");
+    String nameAuthor = scanner.nextLine();
+    System.out.print("E-mail: ");
+    String emailAuthor = scanner.nextLine();
+    System.out.print("Nacionalidade: ");
+    String nationalityAuthor = scanner.nextLine();
+    return new Author(nameAuthor, emailAuthor, nationalityAuthor);
+  }
+
+  private static void registerLoanBook(Library myLibrary, Scanner scanner) {
+    System.out.println("=== Empreśtimo ===");
+    System.out.print("Título do livro: ");
+    String titleBook = scanner.nextLine();
+    System.out.print("Apelido do comodatário: ");
+    String nickNameUser = scanner.nextLine();
+
+    myLibrary.lendBook(titleBook, nickNameUser);
+  }
 }
